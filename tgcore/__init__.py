83a76a134c374294cbcddf8f235d4702b41c6715
Randy SL <killerx@randydev.my.id>
2026-02-17 18:28:14 +0700

    Update

--- tgcore/__init__.py
+++ tgcore/__init__.py
@@ -1,1 +1,17 @@
+# Copyright 2026 Randy W
+# Licensed under the Apache License, Version 2.0
 
+"""
+Github Author: https://github.com/TeamKillerX/
+Code: @zxyeor
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+"""
+
+from .client import Client
+
+__all__ = ["Client"]
