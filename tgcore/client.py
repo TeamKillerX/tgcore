3c35d630e5ba5432d1ca4e4d60f56e325ba71579
Randy SL <killerx@randydev.my.id>
2026-02-18 20:39:34 +0700

    Add keyboard

--- tgcore/client.py
+++ tgcore/client.py
@@ -17,7 +17,7 @@ from __future__ import annotations
 import os
 from typing import Optional
 
-from .core import CoreBotAuth
+from .core import CoreBotAuth, KeyboardBuilder
 from .telegram_namespace import TelegramNamespace
 
 
@@ -32,3 +32,4 @@ class Client(CoreBotAuth):
             )
         super().__init__(api_key, **kwargs)
         self.telegram = TelegramNamespace(self)
+        self.keyboard = KeyboardBuilder(self)
