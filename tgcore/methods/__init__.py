0c327eb6c7e988646b6b40227eaaac9b9cb9c214
Randy SL <killerx@randydev.my.id>
2026-02-19 11:54:21 +0700

    Refactor Methods class by removing unused code
    Removed unused import and constructor from Methods class.

--- tgcore/methods/__init__.py
+++ tgcore/methods/__init__.py
@@ -1,7 +1,4 @@
-from ..core import CoreBotAuth
 from .stickers import Stickers
-# from .messages import Messages
 
 class Methods(Stickers):
-    def __init__(self, client: CoreBotAuth):
-        self._client = client
+    pass
