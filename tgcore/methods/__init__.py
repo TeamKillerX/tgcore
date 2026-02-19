7cb3bcbec29ca5db9dae351c9d05a8d3dcd57507
Randy SL <killerx@randydev.my.id>
2026-02-19 11:48:44 +0700

    Add Methods class to initialize with CoreBotAuth

--- tgcore/methods/__init__.py
+++ tgcore/methods/__init__.py
@@ -1,1 +1,7 @@
+from .core import CoreBotAuth
+from .stickers import Stickers
+# from .messages import Messages
 
+class Methods(Stickers):
+    def __init__(self, client: CoreBotAuth):
+        self._client = client
