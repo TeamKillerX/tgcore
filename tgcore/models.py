5200592d7a02bd14a1e6181dbab56ca2b38e17f0
Randy SL <killerx@randydev.my.id>
2026-02-19 13:22:49 +0700

    Add User and GetMeResponse models

---
+++ tgcore/models.py
@@ -0,0 +1,24 @@
+# Copyright 2026 Randy W
+# Licensed under the Apache License, Version 2.0
+
+# Github Author: https://github.com/TeamKillerX/
+# Code: @zxyeor
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+# http://www.apache.org/licenses/LICENSE-2.0
+
+from pydantic import BaseModel
+from typing import Optional
+
+class User(BaseModel):
+    id: int
+    is_bot: bool
+    first_name: str
+    username: Optional[str] = None
+
+class GetMeResponse(BaseModel):
+    ok: bool
+    data: User
