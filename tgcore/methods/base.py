bf467555a50053807236445f00983fb7fb90762b
Randy SL <killerx@randydev.my.id>
2026-02-19 11:52:23 +0700

    Add BaseMethod class to base.py

---
+++ tgcore/methods/base.py
@@ -0,0 +1,15 @@
+# Copyright 2026 Randy W
+# Licensed under the Apache License, Version 2.0
+
+# Github Author: https://github.com/TeamKillerX/
+# Code: @zxyeor
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+# http://www.apache.org/licenses/LICENSE-2.0
+
+class BaseMethod:
+    def __init__(self, client):
+        self._client = client
