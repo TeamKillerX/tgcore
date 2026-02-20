158024398693946b00d7f74f2015249351e95afd
Randy SL <killerx@randydev.my.id>
2026-02-20 17:33:32 +0700

    Create copy.py

---
+++ tgcore/methods/copy.py
@@ -0,0 +1,31 @@
+# Copyright 2026 Randy W
+# Licensed under the Apache License, Version 2.0
+
+# Github Author: https://github.com/TeamKillerX/
+# Code: @zxyeor
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+# http://www.apache.org/licenses/LICENSE-2.0
+
+from ..core import RequestCall
+from .base import BaseMethod
+
+class Copy(BaseMethod):
+    def copyMessages(self, **kw):
+        return RequestCall(
+            self._client,
+            "POST",
+            "/api/v2/copyMessages",
+            kw
+        )
+
+    def copyMessage(self, **kw):
+        return RequestCall(
+            self._client,
+            "POST",
+            "/api/v2/copyMessage",
+            kw
+        )
