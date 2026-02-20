7d4762cc3f92480808fcef426947e668fbeea780
Randy SL <killerx@randydev.my.id>
2026-02-20 17:43:14 +0700

    _

--- tgcore/methods/__init__.py
+++ tgcore/methods/__init__.py
@@ -1,6 +1,22 @@
 from .message import Message
 from .stickers import Stickers
+from .approve import Approve
+from .ban import Ban
+from .chat import Chat
+from .copy import Copy
+from .deletemsg import DeleteMsg
+from .forward import Forward
+from .mute import Mute
 
-
-class Methods(Stickers, Message):
+class Methods(
+    Message,
+    Stickers,
+    Approve,
+    Ban,
+    Chat,
+    Copy,
+    DeleteMsg,
+    Forward,
+    Mute
+):
     pass
