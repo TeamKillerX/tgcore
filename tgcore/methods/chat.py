97dd4f5565639b5e6e1e2f119e2fd46f333d1b13
Randy SL <killerx@randydev.my.id>
2026-02-20 17:22:02 +0700

    Create chat.py

---
+++ tgcore/methods/chat.py
@@ -0,0 +1,79 @@
+# Copyright 2026 Randy W
+# Licensed under the Apache License, Version 2.0
+
+# Github Author: https://github.com/TeamKillerX/
+# Code: @zxyeor
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+# http://www.apache.org/licenses/LICENSE-2.0
+
+from ..core import RequestCall
+from .base import BaseMethod
+
+
+class Chat(BaseMethod):
+    def getChat(self, **kw):
+        return RequestCall(
+            _client=self._client,
+            _method="GET",
+            _path="/api/v2/getChat",
+            _params=kw
+        )
+
+    def getChatAdministrators(self, **kw):
+        return RequestCall(
+            self._client,
+            "POST",
+            "/api/v2/getChatAdministrators",
+            kw
+        )
+
+    def getChatMember(self, **kw):
+        return RequestCall(
+            self._client,
+            "POST",
+            "/api/v2/getChatMember",
+            kw
+        )
+
+    def exportChatInviteLink(self, **kw):
+        return RequestCall(
+            self._client,
+            "POST",
+            "/api/v2/exportChatInviteLink",
+            kw
+        )
+    def banChatMember(self, **kw):
+        return RequestCall(
+            self._client,
+            "POST",
+            "/api/v2/banChatMember",
+            kw
+        )
+
+    def banChatSenderChat(self, **kw):
+        return RequestCall(
+            self._client,
+            "POST",
+            "/api/v2/banChatSenderChat",
+            kw
+        )
+
+    def restrictChatMember(self, **kw):
+        return RequestCall(
+            self._client,
+            "POST",
+            "/api/v2/restrictChatMember",
+            kw
+        )
+
+    def approveChatJoinRequest(self, **kw):
+        return RequestCall(
+            self._client,
+            "POST",
+            "/api/v2/approveChatJoinRequest",
+            kw
+        )
