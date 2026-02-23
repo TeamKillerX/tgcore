5a668d3b9a89097876ea59788555e1c16f3f8902
Randy SL <killerx@randydev.my.id>
2026-02-24 04:32:41 +0700

    Create topic.py

---
+++ tgcore/methods/topic.py
@@ -0,0 +1,40 @@
+# Copyright 2026 Randy W
+# Licensed under the Apache License, Version 2.0
+
+# Github Author: https://github.com/TeamKillerX/
+# Code: @zxyeor
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+# http://www.apache.org/licenses/LICENSE-2.0
+
+from ..core import RequestCall
+from .base import BaseMethod
+
+
+class Topic(BaseMethod):
+    def editForumTopic(self, **kw):
+        return RequestCall(
+            self._client,
+            "POST",
+            "/api/v2/editForumTopic",
+            kw
+        )
+        
+    def editGeneralForumTopic(self, **kw):
+        return RequestCall(
+            self._client,
+            "POST",
+            "/api/v2/editGeneralForumTopic",
+            kw
+        )
+
+    def hideGeneralForumTopic(self, **kw):
+        return RequestCall(
+            self._client,
+            "POST",
+            "/api/v2/hideGeneralForumTopic",
+            kw
+        )
