94155b485a3fd94912272d9d2ae86643977b5df2
Randy SL <killerx@randydev.my.id>
2026-02-23 18:59:22 +0700

    Create fastapi-tgcore.py

---
+++ examples/fastapi-tgcore.py
@@ -0,0 +1,25 @@
+# Optional: https://core.telegram.org/bots/api
+# Optional: webhook, support proxy, enterprise-grade
+# Learning logic first
+# Easy beginner python
+
+from fastapi import FastAPI
+from tgcore import Client
+
+app = FastAPI()
+tg = Client()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "Word"}
+
+@app.post("/sendMessage")
+async def send(chat_id: int, text: str):
+    return await tg.raw.sendMessage(
+        chat_id=chat_id,
+        text=text,
+        # add another
+    ).execute()
+
+# How to change the Base URL in TGCore
+tg.base_url = "https://easy-your-example.com"
