884836f1e740a7364932f4294ffc0bd5370a8814
Randy SL <killerx@randydev.my.id>
2026-02-24 16:16:52 +0700

    Create gifts.py

---
+++ tgcore/methods/gifts.py
@@ -0,0 +1,32 @@
+# Copyright 2026 Randy W
+# Licensed under the Apache License, Version 2.0
+
+# Github Author: https://github.com/TeamKillerX/
+# Code: @zxyeor
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+# http://www.apache.org/licenses/LICENSE-2.0
+
+from ..core import RequestCall
+from .base import BaseMethod
+
+
+class Gifts(BaseMethod):
+    def getBusinessAccountGifts(self, **kw):
+        return RequestCall(
+            self._client,
+            "POST",
+            "/api/v2/getBusinessAccountGifts",
+            kw
+        )
+
+    def getAvailableGifts(self, **kw):
+        return RequestCall(
+            self._client,
+            "POST",
+            "/api/v2/getAvailableGifts",
+            kw
+        )
