88bab0ec2ecb3e7bc3a2c9080d0d6cfaa9c335ea
Randy SL <killerx@randydev.my.id>
2026-03-10 14:43:34 +0700

    _

--- tgcore/methods/__init__.py
+++ tgcore/methods/__init__.py
@@ -3,7 +3,6 @@ from ._ban import Ban
 from ._chat import Chat
 from ._copy import Copy
 from ._delete import Delete
-from ._downloader import Downloader
 from ._edit import Edit
 from ._files import File
 from ._forward import Forward
