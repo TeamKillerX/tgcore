6078519ea26eff9d0fcd93e91eaf09ee468c635c
Randy SL <killerx@randydev.my.id>
2026-03-10 14:36:00 +0700

    Create _pinterest.py

---
+++ tgcore/platform/_pinterest.py
@@ -0,0 +1,23 @@
+# Copyright 2026 Randy W
+# Licensed under the Apache License, Version 2.0
+
+# Github Author: https://github.com/TeamKillerX/
+# Code: @zxyeor
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+# http://www.apache.org/licenses/LICENSE-2.0
+
+from ..core import RequestCall
+from ..methods.base import BaseMethod
+
+class Pinterest(BaseMethod):
+    def download(self, **kw):
+        return RequestCall(
+            self._client,
+            "POST",
+            "/api/web/pinterest/download",
+            kw
+        )
