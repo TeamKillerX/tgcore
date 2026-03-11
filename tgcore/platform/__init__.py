b250a57c914e95a50696d9ae98bf90b7e4c6bd75
Randy SL <killerx@randydev.my.id>
2026-03-12 06:49:25 +0700

    _

--- tgcore/platform/__init__.py
+++ tgcore/platform/__init__.py
@@ -9,6 +9,6 @@ class Platform:
     def __init__(self, client):
         self.facebook = Facebook(client)
         self.tiktok = TikTok(client)
-        self.tools = AllTools(self)
+        self.tools = AllTools(client)
         self.pinterest = Pinterest(client)
         self.blackforest = Blackforest(client)
