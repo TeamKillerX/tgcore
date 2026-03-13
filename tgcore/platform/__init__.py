f9660d6c4f88565a89a84b12e2a3239c6cfd9946
Randy SL <killerx@randydev.my.id>
2026-03-13 15:22:47 +0700

    Update

--- tgcore/platform/__init__.py
+++ tgcore/platform/__init__.py
@@ -3,7 +3,8 @@ from ._facebook import Facebook
 from ._pinterest import Pinterest
 from ._tiktok import TikTok
 from ._tools import AllTools
-
+from ._capcut import Capcut
+from ._threads import Threads
 
 class Platform:
     def __init__(self, client):
@@ -12,3 +13,5 @@ class Platform:
         self.tools = AllTools(client)
         self.pinterest = Pinterest(client)
         self.blackforest = Blackforest(client)
+        self.capcut = Capcut(client)
+        self.threads = Threads(client)
