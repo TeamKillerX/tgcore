e6ccfb860095588da5b7f7d919861dbc6480b645
Randy Architect <killerx@randydev.my.id>
2026-03-25 16:35:10 +0700

    Update __init__.py

--- tgcore/default/__init__.py
+++ tgcore/default/__init__.py
@@ -1,3 +1,13 @@
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-class-docstring
+# pylint: disable=missing-function-docstring
+# pylint: disable=too-many-public-methods
+# pylint: disable=line-too-long
+# pylint: disable=protected-access
+# pylint: disable=undefined-variable
+# pylint: disable=too-few-public-methods
+# pylint: disable=invalid-name
+
 from ._default import Default
 
 
