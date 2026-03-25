be5b358e8f24fa9af893f662f327e3d4b4256387
Randy Architect <killerx@randydev.my.id>
2026-03-25 16:13:33 +0700

    _

--- tgcore/telegram_namespace.py
+++ tgcore/telegram_namespace.py
@@ -1,6 +1,9 @@
 # pylint: disable=missing-module-docstring
 # pylint: disable=missing-class-docstring
 # pylint: disable=missing-function-docstring
+# pylint: disable=too-many-public-methods
+# pylint: disable=line-too-long
+# pylint: disable=protected-access
 
 # Copyright 2026 Randy W
 # Licensed under the Apache License, Version 2.0
