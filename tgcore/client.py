736e85965b340720d9b5a7cfa484c40c1456b63a
Randy Architect <killerx@randydev.my.id>
2026-03-25 08:31:58 +0700

    _

--- tgcore/client.py
+++ tgcore/client.py
@@ -1,16 +1,14 @@
 # Copyright 2026 Randy W
 # Licensed under the Apache License, Version 2.0
 
-"""
-Github Author: https://github.com/TeamKillerX/
-Code: @zxyeor
+# Github Author: https://github.com/TeamKillerX/
+# Code: @zxyeor
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 
-http://www.apache.org/licenses/LICENSE-2.0
-"""
+# http://www.apache.org/licenses/LICENSE-2.0
 
 from __future__ import annotations
 
@@ -34,8 +32,12 @@ class Client(CoreBotAuth):
         **kw
     ):
         api_key = api_key or os.getenv("TGCORE_API_KEY")
-        super().__init__(api_key, bearer_token, is_bearer, **kw)
-
+        super().__init__(
+            api_key=api_key,
+            bearer_token=bearer_token,
+            is_bearer=is_bearer,
+            **kw
+        )
         self.raw = Methods(self)
         self.ai = Chats(self)
         self.use = DefaultMethod(self)
