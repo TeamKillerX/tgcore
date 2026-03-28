4cd086de14d2b73738014319698a35001ca951f0
Randy Architect <killerx@randydev.my.id>
2026-03-28 12:54:56 +0700

    _

--- tgcore/monitors/__init__.py
+++ tgcore/monitors/__init__.py
@@ -1,1 +1,17 @@
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-class-docstring
+# pylint: disable=missing-function-docstring
+# pylint: disable=too-many-public-methods
+# pylint: disable=line-too-long
+# pylint: disable=protected-access
+# pylint: disable=undefined-variable
+# pylint: disable=too-few-public-methods
+# pylint: disable=invalid-name
 
+from ._monitor import MonitorsGet, MonitorsCreate
+
+class UptimeRobot(
+    MonitorsGet,
+    MonitorsCreate
+):
+    pass
